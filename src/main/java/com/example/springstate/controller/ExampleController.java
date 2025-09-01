2c8bbc492cd3ddea20698758b2726a646dc09a7b
daeunkim701 <lionboot2025@gmail.com>
2025-09-01 10:09:04 +0900

    result

--- src/main/java/com/example/springstate/controller/ExampleController.java
+++ src/main/java/com/example/springstate/controller/ExampleController.java
@@ -3,6 +3,10 @@ package com.example.springstate.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller // 스캔하는 애
 public class ExampleController {
@@ -11,5 +15,16 @@ public class ExampleController {
         model.addAttribute("message", "안녕 나는 모델이야");
         model.addAttribute("username", "모델");
         return "hello";
+    }
+    @PostMapping("/save-data")
+    public String saveData(@RequestParam String data, Model model) {
+        Random rd = new Random();
+        boolean success = rd.nextBoolean(); // 반반 확률
+        if (success) {
+            model.addAttribute("message", data + "가 성공적으로 저장되었습니다.");
+        } else {
+            model.addAttribute("message", data + "의 저장에 실패했습니다.");
+        }
+        return "result";
     }
 }
